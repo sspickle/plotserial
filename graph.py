0f59c2889e20cc4d38f37fd7bf3c6b3185798191
Steve Spicklemire <steve@spvi.com>
2022-01-17 17:52:20 -0500

    close

--- graph.py
+++ graph.py
@@ -44,6 +44,8 @@ def calcCurrents(k, frame):
 class CanvasFrame(wx.Frame):
     def __init__(self):
         super().__init__(None, -1, 'CanvasFrame', size=(550, 350))
+        
+        self.paused = False
 
         self.lock = RLock()
         self.data = {}
@@ -57,6 +59,21 @@ class CanvasFrame(wx.Frame):
         self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.EXPAND)
         self.SetSizer(self.sizer)
         self.Fit()
+        
+        self.hbox = wx.BoxSizer(wx.HORIZONTAL)
+        
+        self.clearButton = wx.Button(self, label="Clear")
+        self.saveButton = wx.Button(self, label="Save Data")
+        self.pauseButton = wx.Button(self, label="Pause")
+        self.hbox.Add(self.clearButton, 0)
+        self.hbox.Add(self.saveButton, 20)
+        self.hbox.Add(self.pauseButton, 20)
+
+        self.sizer.Add(self.hbox, 0, wx.LEFT | wx.BOTTOM)
+
+        self.clearButton.Bind(wx.EVT_BUTTON, self.OnClear)
+        self.saveButton.Bind(wx.EVT_BUTTON, self.OnSave)
+        self.pauseButton.Bind(wx.EVT_BUTTON, self.OnPause)
 
         self.add_toolbar()  # comment this out for no toolbar
         self.update_plot()
@@ -82,6 +99,13 @@ class CanvasFrame(wx.Frame):
     def OnClear(self, evt):
         self.data = {}
         
+    def OnPause(self, evt):
+        self.paused = not self.paused
+        if self.paused:
+            self.pauseButton.SetLabel("Resume")
+        else:
+            self.pauseButton.SetLabel("Pause")
+        
     def OnSave(self, evt):
         self.lock.acquire()
         resultFile = open('Unknown_Data.py', 'w')
@@ -101,18 +125,19 @@ class CanvasFrame(wx.Frame):
         self.lock.release()
         
     def update_plot(self):
-        self.lock.acquire()
-        self.axes.clear()
-        #self.axes.plot(range(100),range(100), 'b-')
-        for k in sorted(list(self.data.keys())):
-            currObjs = calcCurrents(k,self)
-            ibMean = currObjs['iBase'].mean()
-            self.axes.plot(currObjs['vc'], 1000*currObjs['iColl'],label="$I_{b}$ = %.1f $\mu$A" % (ibMean*1e6))
-        self.axes.grid()
-        self.axes.legend()
-        self.axes.set_xlabel('Vc (V)')
-        self.axes.set_ylabel('Ic (mA)')
-        self.lock.release()
+        if not self.paused:
+            self.lock.acquire()
+            self.axes.clear()
+            #self.axes.plot(range(100),range(100), 'b-')
+            for k in sorted(list(self.data.keys())):
+                currObjs = calcCurrents(k,self)
+                ibMean = currObjs['iBase'].mean()
+                self.axes.plot(currObjs['vc'], 1000*currObjs['iColl'],label="$I_{b}$ = %.1f $\mu$A" % (ibMean*1e6))
+            self.axes.grid()
+            self.axes.legend()
+            self.axes.set_xlabel('Vc (V)')
+            self.axes.set_ylabel('Ic (mA)')
+            self.lock.release()
 
     def OnPaint(self, event):
         self.update_plot()
